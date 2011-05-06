77430e42ad3ce7bad5f0d5dd6689da59720753db
Peter Atechian <retep998@verizon.net>
2011-05-06 19:44:57 -0400

    Getting rid of old stuff in preparation for new stuff.

--- Graphics.h
+++
@@ -1,8 +0,0 @@
-///////////////////////////////////////////////
-// This file is part of NoLifeStory.         //
-// Please see Global.h for more information. //
-///////////////////////////////////////////////
-
-extern SDL_Surface* screen;
-void unload_sprites();
-void init_graphics();
