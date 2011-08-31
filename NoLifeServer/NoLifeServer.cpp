ee1d6de20d243ca1d8ce654ab4b2e46502ad77de
natebender <var.nate.bender@gmail.com>
2011-08-31 00:03:03 -0400

    binding that port like a boss!
    Signed-off-by: natebender <var.nate.bender@gmail.com>

--- NoLifeServer/NoLifeServer.cpp
+++ NoLifeServer/NoLifeServer.cpp
@@ -3,14 +3,34 @@
 // Please see SuperGlobal.h for more information. //
 ////////////////////////////////////////////////////
 #include "Global.h"
-
-int main(int argc, char **argv) {//Not much to do here
+unsigned short port = 10273;
+int main(int argc, char **argv) {
 #ifdef NLS_WINDOWS
 	SetConsoleTitleA("NoLifeServer::Console");
 #endif
 	cout << "Loading NoLifeServer..." << endl;
 	vector<string> strs(argv, argv+argc);
-	//Actually run NLS here
+	
+	sf::TcpSocket Client;
+	sf::TcpListener Listener;
+
+	while(true) {
+		Listener.Listen(port);
+		if(!Listener.Listen(port)) {
+			cout << "Failed to listen on port 10273" << endl;
+		}else {
+			cout << "Waiting for connections on port 10273" << endl;
+		}
+		if(Listener.Accept(Client) != sf::TcpSocket::Done) {
+			cout << "Client could not connect" << endl;
+		} else {
+			cout << "Client connected successfully" << endl;
+			//Do something with client
+		}
+		/*if(Listener.IsBlocking() == true) {
+			cout << "The socket is in blocking mode." << endl;
+		}*/
+	}
 	NLS::Init(strs);
 	while (NLS::Loop()) {}
 	NLS::Unload();
--- NoLifeStory/Network.cpp
+++ NoLifeStory/Network.cpp
@@ -9,5 +9,6 @@ sf::TcpSocket NLS::Network::Socket;
 void NLS::Network::Init() {
 	if (Socket.Connect("localhost", 10273) != sf::Socket::Done) {
 		cerr << "WARNING: Failed to connect to server" << endl;
+		// Not to worry! Professor Nubcake is here! I'm working on the server finally F3..
 	}
 }
