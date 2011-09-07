0a4a505291dd160decfeb2f62176a080185dc382
Retep998 <retep998@verizon.net>
2011-09-06 22:44:01 -0400

    Documentation for all! I need to actually add doxy style comments now.
    Signed-off-by: Retep998 <retep998@verizon.net>

---
+++ navtree.js
@@ -0,0 +1,319 @@
+var NAVTREE =
+[
+  [ "NoLifeStory", "index.html", [
+    [ "Class List", "annotated.html", [
+      [ "NLS::_Time", "class_n_l_s_1_1___time.html", null ],
+      [ "NLS::Console", "class_n_l_s_1_1_console.html", null ],
+      [ "NLS::WZ::Directory", "class_n_l_s_1_1_w_z_1_1_directory.html", null ],
+      [ "NLS::Endl_", "class_n_l_s_1_1_endl__.html", null ],
+      [ "NLS::WZ::ExtendedProperty", "class_n_l_s_1_1_w_z_1_1_extended_property.html", null ],
+      [ "NLS::WZ::File", "class_n_l_s_1_1_w_z_1_1_file.html", null ],
+      [ "NLS::Foothold", "class_n_l_s_1_1_foothold.html", null ],
+      [ "NLS::WZ::Header", "class_n_l_s_1_1_w_z_1_1_header.html", null ],
+      [ "NLS::WZ::Image", "class_n_l_s_1_1_w_z_1_1_image.html", null ],
+      [ "NLS::LoginServer", "class_n_l_s_1_1_login_server.html", null ],
+      [ "NLS::Node", "class_n_l_s_1_1_node.html", null ],
+      [ "NLS::NodeData", "class_n_l_s_1_1_node_data.html", null ],
+      [ "NLS::Sprite", "class_n_l_s_1_1_sprite.html", null ],
+      [ "NLS::SpriteData", "class_n_l_s_1_1_sprite_data.html", null ],
+      [ "NLS::Stream", "class_n_l_s_1_1_stream.html", null ],
+      [ "NLS::WZ::SubProperty", "class_n_l_s_1_1_w_z_1_1_sub_property.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Members", "functions.html", null ],
+    [ "Namespace List", "namespaces.html", [
+      [ "NLS", "namespace_n_l_s.html", null ],
+      [ "NLS::Graphics", "namespace_n_l_s_1_1_graphics.html", null ],
+      [ "NLS::Map", "namespace_n_l_s_1_1_map.html", null ],
+      [ "NLS::Network", "namespace_n_l_s_1_1_network.html", null ],
+      [ "NLS::WZ", "namespace_n_l_s_1_1_w_z.html", null ]
+    ] ],
+    [ "Namespace Members", "namespacemembers.html", null ],
+    [ "File List", "files.html", [
+      [ "Resource.h", "_resource_8h.html", null ],
+      [ "SuperGlobal.h", "_super_global_8h.html", null ],
+      [ "NoLifeServer/Global.h", "_no_life_server_2_global_8h.html", null ],
+      [ "NoLifeServer/LoginServer.cpp", "_login_server_8cpp.html", null ],
+      [ "NoLifeServer/LoginServer.h", "_login_server_8h.html", null ],
+      [ "NoLifeServer/NoLifeServer.cpp", "_no_life_server_8cpp.html", null ],
+      [ "NoLifeServer/Server.cpp", "_server_8cpp.html", null ],
+      [ "NoLifeServer/Server.h", "_server_8h.html", null ],
+      [ "NoLifeStory/Client.cpp", "_client_8cpp.html", null ],
+      [ "NoLifeStory/Client.h", "_client_8h.html", null ],
+      [ "NoLifeStory/Console.cpp", "_console_8cpp.html", null ],
+      [ "NoLifeStory/Console.h", "_console_8h.html", null ],
+      [ "NoLifeStory/Foothold.cpp", "_foothold_8cpp.html", null ],
+      [ "NoLifeStory/Foothold.h", "_foothold_8h.html", null ],
+      [ "NoLifeStory/Global.h", "_no_life_story_2_global_8h.html", null ],
+      [ "NoLifeStory/Graphics.cpp", "_graphics_8cpp.html", null ],
+      [ "NoLifeStory/Graphics.h", "_graphics_8h.html", null ],
+      [ "NoLifeStory/Keys.h", "_keys_8h.html", null ],
+      [ "NoLifeStory/Map.cpp", "_map_8cpp.html", null ],
+      [ "NoLifeStory/Map.h", "_map_8h.html", null ],
+      [ "NoLifeStory/Network.cpp", "_network_8cpp.html", null ],
+      [ "NoLifeStory/Network.h", "_network_8h.html", null ],
+      [ "NoLifeStory/NoLifeStory.cpp", "_no_life_story_8cpp.html", null ],
+      [ "NoLifeStory/Sprite.cpp", "_sprite_8cpp.html", null ],
+      [ "NoLifeStory/Sprite.h", "_sprite_8h.html", null ],
+      [ "NoLifeStory/Time.cpp", "_time_8cpp.html", null ],
+      [ "NoLifeStory/Time.h", "_time_8h.html", null ],
+      [ "NoLifeStory/Utilities.cpp", "_utilities_8cpp.html", null ],
+      [ "NoLifeStory/Utilities.h", "_utilities_8h.html", null ],
+      [ "NoLifeStory/View.cpp", "_view_8cpp.html", null ],
+      [ "NoLifeStory/View.h", "_view_8h.html", null ],
+      [ "NoLifeStory/WZ.cpp", "_w_z_8cpp.html", null ],
+      [ "NoLifeStory/WZ.h", "_w_z_8h.html", null ]
+    ] ],
+    [ "Directories", "dirs.html", [
+      [ "NoLifeServer", "dir_6437b2d0aae65d0d0aac8aba1c641d9a.html", null ],
+      [ "NoLifeStory", "dir_1836b9bb9065288a996b64b85501a89e.html", null ]
+    ] ],
+    [ "File Members", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
